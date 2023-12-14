77ce1cff33f66aae696080ba68ff523bcada130c
Ali Abdelmoaty <68239507+aliabdelmoaty@users.noreply.github.com>
2023-12-14 21:12:40 +0200

    create shape to

--- src/main/java/models/AddContact.java
+++ src/main/java/models/AddContact.java
@@ -1,9 +1,10 @@
 package models;
+
 import java.awt.Color;
 import javax.swing.*;
 
+import utils.RoundJTextField;
 import utils.RoundedBorder;
-
 
 public class AddContact extends JFrame {
 
@@ -22,20 +23,20 @@ public class AddContact extends JFrame {
         this.setResizable(false);
         setTitle("Contacts");
 
-        nameField = new JTextField(20);
-        emailField = new JTextField(20);
-        phoneField = new JTextField(20);
+        nameField = new RoundJTextField("Enter name",30);
+        emailField = new RoundJTextField("Enter email",30);
+        phoneField = new RoundJTextField("Enter phone",30);
         saveButton = new RoundedBorder("Save");
         saveButton.setBackground(Color.RED);
         labelName = new JLabel("Name");
         labelEmail = new JLabel("Email");
         labelPhone = new JLabel("Phone");
+        
         labelName.setBounds(15, 10, 70, 10);
         nameField.setBounds(15, 25, 200, 20);
 
         labelEmail.setBounds(15, 55, 70, 10);
         emailField.setBounds(15, 70, 200, 20);
-
         labelPhone.setBounds(15, 100, 70, 10);
         phoneField.setBounds(15, 115, 200, 20);
 
---
+++ src/main/java/utils/RoundJTextField.java
@@ -0,0 +1,57 @@
+package utils;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Shape;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.geom.RoundRectangle2D;
+
+import javax.swing.JTextField;
+
+public class RoundJTextField extends JTextField {
+    private Shape shape;
+    public RoundJTextField(String hint, int columns) {
+        super(hint,columns);
+        
+        setOpaque(false);
+        setForeground(Color.GRAY);
+        setFont(new Font("Arial", Font.BOLD, 12));
+        addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (getText().equals(hint)) {
+                    setText("");
+                    setForeground(Color.GRAY);
+                }
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (getText().isEmpty()) {
+                    setText(hint);
+                    setForeground(Color.GRAY);
+                }
+            }
+        });
+    }
+
+    protected void paintComponent(Graphics g) {
+        g.setColor(getBackground());
+        g.fillRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 15, 15);
+        super.paintComponent(g);
+    }
+
+    protected void paintBorder(Graphics g) {
+        g.setColor(getForeground());
+        g.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 15, 15);
+    }
+
+    public boolean contains(int x, int y) {
+        if (shape == null || !shape.getBounds().equals(getBounds())) {
+            shape = new RoundRectangle2D.Float(0, 0, getWidth() - 1, getHeight() - 1, 15, 15);
+        }
+        return shape.contains(x, y);
+    }
+}
