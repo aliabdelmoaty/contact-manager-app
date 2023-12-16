2721ea4e66db136f38e8ff93aef504bec89bb9f0
Ali Abdelmoaty <68239507+aliabdelmoaty@users.noreply.github.com>
2023-12-16 12:44:28 +0200

    handle error fail connection

--- src/main/java/logic/SQLServer.java
+++ src/main/java/logic/SQLServer.java
@@ -18,12 +18,13 @@ public class SQLServer {
     private static Statement statement = null;
     private static PreparedStatement preparedStatement = null;
     
-    public static void connectToDatabase() {
+    public static void connectToDatabase()throws HandleErrors{
         try {
             connection = DriverManager.getConnection(JDBC_URL, userName, password);
         } catch (SQLException e) {
             System.out.println("Error connecting to database: " + e.getMessage());
             e.printStackTrace();
+            new HandleErrors("Error connecting to the database. Please check your network connection or server status.");
 
         }
     }
@@ -43,22 +44,24 @@ public class SQLServer {
         }
     }
 
-    public static void insertData(String name, String email, int phone, String address)
-     throws HandleErrors {
+    public static void insertData(String name, String email, int phone, String address) throws HandleErrors {
         try {
             connectToDatabase();
-            preparedStatement = connection
-                    .prepareStatement("INSERT INTO myContacts(name ,email,phone,address) VALUES (?, ?, ?,?)");
-            preparedStatement.setString(1, name);
-            preparedStatement.setString(2, email);
-            preparedStatement.setInt(3, phone);
-            preparedStatement.setString(4, address);
-            preparedStatement.executeUpdate();
+            if (connection != null) {
+                preparedStatement = connection
+                        .prepareStatement("INSERT INTO myContacts(name ,email,phone,address) VALUES (?, ?, ?,?)");
+                preparedStatement.setString(1, name);
+                preparedStatement.setString(2, email);
+                preparedStatement.setInt(3, phone);
+                preparedStatement.setString(4, address);
+                preparedStatement.executeUpdate();
+            } else {
+                throw new HandleErrors(" Please check your network connection or server status.");
+            }
         } catch (SQLException e) {
             System.out.println("Error inserting data: " + e.getMessage());
             e.printStackTrace();
             throw new HandleErrors("Error inserting data: " + e.getMessage());
-            
         } finally {
             closeResources();
         }
